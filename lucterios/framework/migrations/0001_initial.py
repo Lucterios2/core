304378e16ed8cbb7dfd53eff389c11a31c715535
Laurent GAY <l.gay@sd-libre.fr>
2015-03-13 22:20:56 +0100

    fake migration of framework

---
+++ lucterios/framework/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
+
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('sessions', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='LucteriosSession',
+            fields=[
+            ],
+            options={
+                'default_permissions': [],
+                'proxy': True,
+            },
+            bases=('sessions.session',),
+        ),
+    ]
