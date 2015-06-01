ad8b951376bc35ec11879418a6e657a6c0cc4d9b
Laurent GAY <l.gay@sd-libre.fr>
2015-06-01 17:42:24 +0200

    group permission manage for documents

--- lucterios/framework/xferadvance.py
+++ lucterios/framework/xferadvance.py
@@ -116,7 +116,7 @@ class XferAddEditor(XferContainerCustom):
 
 class XferShowEditor(XferContainerCustom):
 
-    def fillresponse(self):
+    def fillresponse(self, action_list=None):
         img = XferCompImage('img')
         img.set_value(icon_path(self))
         img.set_location(0, 0, 1, 5)
@@ -126,7 +126,8 @@ class XferShowEditor(XferContainerCustom):
         lbl.set_location(1, 0)
         self.add_component(lbl)
         self.fill_from_model(1, 0, True)
-        action_list = [('modify', _("Modify"), "images/edit.png", CLOSE_YES), ('print', _("Print"), "images/print.png", CLOSE_NO)]
+        if action_list is None:
+            action_list = [('modify', _("Modify"), "images/edit.png", CLOSE_YES), ('print', _("Print"), "images/print.png", CLOSE_NO)]
         for act_type, title, icon, close in action_list:
             self.add_action(ActionsManage.get_act_changed(self.model.__name__, act_type, title, icon), {'close':close})
         self.add_action(StubAction(_('Close'), 'images/close.png'), {})
