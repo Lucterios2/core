0cc2d3a32c96e82aac0a5e7fdcea10555b52ba2c
Laurent GAY <l.gay@sd-libre.fr>
2015-09-03 17:49:33 +0200

    Perform for windows install

--- utils/install.ps1
+++ utils/install.ps1
@@ -3,47 +3,103 @@
 
 echo "====== install lucterios ======"
 
-echo
-echo "------ download python -------"
-echo
-
-$url = "https://www.python.org/ftp/python/3.4.3/python-3.4.3.amd64.msi"
+$lucterios_path="c:/lucterios2"
+if ($env:PROCESSOR_ARCHITECTURE -eq "AMD64") {
+    $url_python = "https://www.python.org/ftp/python/3.4.3/python-3.4.3.amd64.msi"    
+    $url_lxml = "https://raw.githubusercontent.com/Lucterios2/core/master/packages/lxml-3.4.4-cp34-none-win32.whl"
+    $url_pycrypto = "https://raw.githubusercontent.com/Lucterios2/core/master/packages/pycrypto-2.6.1-cp34-none-win_amd64.whl"
+} else {
+    $url_python = "https://www.python.org/ftp/python/3.4.3/python-3.4.3.msi"
+    $url_lxml = "https://raw.githubusercontent.com/Lucterios2/core/master/packages/lxml-3.4.4-cp34-none-win32.whl"
+    $url_pycrypto = "https://raw.githubusercontent.com/Lucterios2/core/master/packages/pycrypto-2.6.1-cp34-none-win32.whl"
+}
 $python_install = "$env:temp\python.msi"
-$webclient = New-Object System.Net.WebClient
-$webclient.DownloadFile($url,$python_install)
+$lxml_install = "$env:temp\" + (Split-Path $url_lxml -Leaf)
+$pycrypto_install = "$env:temp\" + (Split-Path $url_pycrypto -Leaf)
 
-$url = "https://raw.github.com/pypa/pip/master/contrib/get-pip.py"
-$pip_install = "$env:temp\get-pip.py"
-$webclient = New-Object System.Net.WebClient
-$webclient.DownloadFile($url,$pip_install)
+echo ""
+echo "------ download python -------"
+echo ""
 
-echo
+Invoke-WebRequest -Uri $url_python -OutFile $python_install
+if (!(Test-Path $python_install)) {
+    echo "**** Dowload python failed! *****"
+    exit 1
+}
+
+Invoke-WebRequest -Uri $url_lxml -OutFile $lxml_install
+if (!(Test-Path $lxml_install)) {
+    echo "**** Dowload lxml failed! *****"
+    exit 1
+}
+
+Invoke-WebRequest -Uri $url_pycrypto -OutFile $pycrypto_install
+if (!(Test-Path $pycrypto_install)) {
+    echo "**** Dowload pycrypto failed! *****"
+    exit 1
+}
+
+echo ""
 echo "------ install python -------"
-echo
+echo ""
 
 msiexec /i $python_install /qn
-python $pip_install
-pip install virtualenv
+$env:Path="$env:Path;c:\Python34;c:\Python34\Scripts\"
+pip install -U pip virtualenv
 
-echo
+
+echo ""
 echo "------ configure virtual environment ------"
-echo
+echo ""
 
-mkdir -p c:/lucterios2
-cd c:/lucterios2
-python virtualenv virtual_for_lucterios
+if (!(Test-Path $lucterios_path)) {
+    mkdir -p $lucterios_path
+}
+cd $lucterios_path
+virtualenv virtual_for_lucterios
 
-echo
+if (!(Test-Path $lucterios_path\virtual_for_lucterios\Scripts\activate)) {
+    echo "**** Virtual-Env not created! *****"
+    exit 1
+}
+
+echo ""
 echo "------ install lucterios ------"
-echo
+echo ""
 
-c:/lucterios2/virtual_for_lucterios/Scripts/activate
+.\virtual_for_lucterios\Scripts\activate
+pip install -U $lxml_install $pycrypto_install
 pip install --extra-index-url http://pypi.lucterios.org/simple --trusted-host pypi.lucterios.org -U lucterios-standard
 
-echo
+echo ""
 echo "------ refresh shortcut ------"
-echo
+echo ""
 
+if (Test-Path $lucterios_path\launch_lucterios.ps1) {
+    del $lucterios_path\launch_lucterios.ps1
+}
+echo "#requires -version 4.0" >> $lucterios_path\launch_lucterios.ps1
+echo "" >> $lucterios_path\launch_lucterios.ps1
+echo "echo 'Lucterios GUI launcher'" >> $lucterios_path\launch_lucterios.ps1
+echo "" >> $lucterios_path\launch_lucterios.ps1
+echo "cd $lucterios_path" >> $lucterios_path\launch_lucterios.ps1
+echo "virtual_for_lucterios\Scripts\activate" >> $lucterios_path\launch_lucterios.ps1
+echo "" >> $lucterios_path\launch_lucterios.ps1
+echo "`$env:Path=`"`$env:Path;c:\Python34;c:\Python34\DLLs`"" >> $lucterios_path\launch_lucterios.ps1
+echo "`$env:TCL_LIBRARY='c:\Python34\tcl\tcl8.6'" >> $lucterios_path\launch_lucterios.ps1
+echo "`$env:TK_LIBRARY='c:\Python34\tcl\tcl8.6'" >> $lucterios_path\launch_lucterios.ps1
+echo "python virtual_for_lucterios\Scripts\lucterios_gui.py" >> $lucterios_path\launch_lucterios.ps1
+echo "" >> $lucterios_path\launch_lucterios.ps1
+
+if (Test-Path $env:Public\Desktop\Lucterios.lnk) {
+    del $env:Public\Desktop\Lucterios.lnk
+}
+
+$WshShell = New-Object -ComObject WScript.shell
+$Shortcut = $WshShell.CreateShortcut("$env:Public\Desktop\Lucterios.lnk")
+$Shortcut.TargetPath = "PowerShell.exe -ExecutionPolicy Bypass -File $lucterios_path\launch_lucterios.ps1"
+$Shortcut.IconLocation = "$lucterios_path\virtual_for_lucterios\Lib\site-packages\lucterios\install\lucterios.ico"
+$Shortcut.Save()
 
 echo "============ END ============="
 exit 0
