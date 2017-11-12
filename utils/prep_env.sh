2d3d361e5cf0179c31f9e45a1639ac1cda201e60
Laurent GAY <l.gay@sd-libre.fr>
2017-11-12 17:03:39 +0100

    correct & dependancy

---
+++ utils/prep_env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CurrentPath=$(dirname "$(readlink -f "$0")")
+cd $CurrentPath/../..
+
+grep 'install_requires' */setup.py  | sed 's|.*install_requires=\[||g' | sed 's|\],||g' | sed 's|, |\n|g' | sed 's|"||g' | egrep -v 'lucterios|diacamma' | sort | uniq > requirement.txt
+echo "wheel" >> requirement.txt
+echo "Sphinx" >> requirement.txt
+
+[ -d virt ] && rm -rf
+
+python3 $(which virtualenv) virt
+
+source virt/bin/activate
+
+pip install -U -r requirement.txt
+
+deactivate
