a31dedfd2d8bc435012f24eab1377e8a5cd5040f
Laurent GAY <l.gay@sd-libre.fr>
2019-01-27 22:04:18 +0100

    printing correction

--- lucterios/framework/reporting.py
+++ lucterios/framework/reporting.py
@@ -72,7 +72,12 @@ def extract_text(xmltext):
     para_text = para_text[para_text.find('>') + 1:]
     para_text = para_text.replace('</TEXT>', '')
     para_text = para_text.replace("\\n", " ")
+    para_text = para_text.replace("\\t", " ")
+    para_text = para_text.replace("\\r", " ")
+    para_text = para_text.replace("\\'", "&#130;")
     para_text = para_text.replace("'", "")
+    para_text = para_text.replace("&#130;", "'")
+    para_text = para_text.replace('"', "&#34;")
     para_text = re.sub(pattern, ' ', para_text)
     para_text = para_text.strip()
     return para_text
