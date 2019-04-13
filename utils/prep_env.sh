18881f3901da8925163101153ab3065cccc8d2ff
Laurent GAY <l.gay@sd-libre.fr>
2019-04-13 19:23:46 +0200

    correct pep8 build

--- utils/prep_env.sh
+++ utils/prep_env.sh
@@ -4,6 +4,7 @@ CurrentPath=$(dirname "$(readlink -f "$0")")
 cd $CurrentPath/../..
 
 grep 'install_requires' */setup.py  | sed 's|.*install_requires=\[||g' | sed 's|\],||g' | sed 's|, |\n|g' | sed 's|"||g' | egrep -v 'lucterios|diacamma' | sort | uniq > requirement.txt
+echo "pycodestyle" >> requirement.txt
 echo "coverage" >> requirement.txt
 echo "wheel" >> requirement.txt
 echo "Sphinx" >> requirement.txt
